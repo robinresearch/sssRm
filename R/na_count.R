686e08768881fe46d62e42f2182dc2f22f129310
robinresearch <robin.bergh@live.nl>
2023-06-22 15:08:20 +0200

    Documentation updated

--- R/na_count.R
+++ R/na_count.R
@@ -1,3 +1,14 @@
+#' Count missing values
+#'
+#' @param x A vector or list
+#'
+#' @return A numerical vector
+#' @export
+#'
+#' @examples
+#' na_count(c(1, 2, NA, NA))
+#'
+#'
 na_count <- function(x) {
   base::sum(base::is.na(x))
 }
